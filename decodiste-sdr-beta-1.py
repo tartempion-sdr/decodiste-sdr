0df919cce74376407ee7565bfa3e0eb6e1b5ea00
tartempion-sdr <mammuth4639@gmail.com>
2021-11-02 09:39:37 +0100

    iconbitmap1

--- decodiste-sdr-beta-1.py
+++ decodiste-sdr-beta-1.py
@@ -58,7 +58,7 @@ fenetreprincipale.configure(bg='grey')
 # icone #
 #########
 
-#fenetreprincipale.iconbitmap('@/home/tartempion/Documents/python/sdr/antenne.xbm')
+fenetreprincipale.iconbitmap('@/antenne.xbm')
 
 
 #####################################################################
